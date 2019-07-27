74b9751964bbfbac5f8d5fce8cae107d96c9b588
Andre Rocha <andrefmrocha@live.com.pt>
2019-07-27 18:29:56 +0100

    Repository and respective functions

--- test/meals_repository_tests.js
+++ test/meals_repository_tests.js
@@ -1,22 +1,33 @@
-/* eslint-disable no-unused-vars */
 const mealsRepository = require("./../lib/repository/meals_repository");
-const should = require("chai").should();
-const mongoose = require("mongoose");
+const chai = require("chai");
+const should = chai.should();
 
-describe("Testing save and find function from repository", function() {
+describe("Testing repository functions", function() {
+    let meal = {
+        soup: "batata frita",
+        meat: "Vitela assada",
+        fish: "Sardinhas",
+        veggie: "Salada",
+        cantine: "FEUP",
+        day: new Date("1995-12-17T03:24:00"),
+        lunch: true
+    };
     it("should save the model to the repository and then retrieve it properly", async function(){
-        let meal = {
-            soup: "batata frita",
-            meat: "Vitela assada",
-            fish: "Sardinhas",
-            veggie: "Salada",
-            cantine: "FEUP",
-            day: new Date("1995-12-17T03:24:00"),
-            lunch: true
-        };
-        mealsRepository.save(meal);
+        await mealsRepository.save(meal);
         let savedMeal = await mealsRepository.find(meal);
-        console.log(savedMeal);
-        // should.equal(savedMeal, !null);
+        should.equal(savedMeal.soup, meal.soup);
+        should.equal(savedMeal.meat, meal.meat);
+        should.equal(savedMeal.fish, meal.fish);
+        should.equal(savedMeal.veggie, meal.veggie);
+        should.equal(savedMeal.cantine, meal.cantine);
+        should.equal(savedMeal.lunch, meal.lunch);
+        mealsRepository.delete(meal);
+    });
+
+    it("should have the model at first but then it should have been deleted", async function(){
+        meal.meat = "Vitela";
+        await mealsRepository.save(meal);
+        let deletion = await mealsRepository.delete(meal);
+        should.equal(deletion.deletedCount, 1);
     });
 });
