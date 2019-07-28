0f0f7b4c9eb54570d7bca6b0b96cc9fbbd1e52df
GambuzX <mg_mesquita@hotmail.com>
2019-07-28 11:56:24 +0100

    Writing remote pdfs localy temporarily

--- processors/pdf_reader.js
+++ processors/pdf_reader.js
@@ -1,12 +1,37 @@
 const pdfURL = require("./html_parser");
 const pdfParse = require("./../lib/interactors/pdf_parser");
 const crawler = require("crawler-request");
+const request = require("request-promise");
+const fs = require("fs");
 
 const cantineUrl = "https://sigarra.up.pt/sasup/pt/web_base.gera_pagina?P_pagina=265689";
     
 async function pdfParsing() {
     let urls = await pdfURL.getURLs(cantineUrl);
-    urls.forEach(elem => {
+
+    for (let i = 0; i < urls.length; i++) {
+
+        let pdf_file = "file" + (i+1).toString() + ".pdf"
+
+        let elem = urls[i];
+        let pdfContent = await request.get({uri: elem});
+
+        try {
+            fs.writeFileSync(pdf_file, pdfContent);
+        } catch(err) {
+            console.log(err);
+        }
+        
+        fs.readFile(pdf_file, function(err, data) {
+            console.log("data: " , data);
+            console.log("\n");
+        });
+
+        fs.unlink(pdf_file, function(err) {
+            if (err) console.log(err);
+        });
+
+        /*
         crawler(elem).then(function(response){
             if(!response.error && response.status === 200) {//TODO: Check which sort of menu it is and act accordingly
                 pdfParse.newReadMeals(response);
@@ -14,8 +39,9 @@ async function pdfParsing() {
             else {
                 console.log("error in url: ", response.url);
             }
-        });
-    });
+        });*/
+
+    }
 }
 
 pdfParsing();
