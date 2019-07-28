6498c5889104c451afc5ae2eac4d0df60f8a2641
GambuzX <mg_mesquita@hotmail.com>
2019-07-28 13:59:00 +0100

    Using pdf-extract for text and ocr

--- processors/pdf_reader.js
+++ processors/pdf_reader.js
@@ -3,33 +3,58 @@ const pdfParse = require("./../lib/interactors/pdf_parser");
 const crawler = require("crawler-request");
 const request = require("request-promise");
 const fs = require("fs");
+const pdf_extract = require("pdf-extract");
 
 const cantineUrl = "https://sigarra.up.pt/sasup/pt/web_base.gera_pagina?P_pagina=265689";
     
 async function pdfParsing() {
     let urls = await pdfURL.getURLs(cantineUrl);
 
+    let options = {
+        type: 'ocr',
+        ocr_flags: [
+            '-psm 1',       // automatically detect page orientation
+            'alphanumeric'  // only output ascii characters
+          ]
+    }
+
     for (let i = 0; i < urls.length; i++) {
 
-        let pdf_file = "file" + (i+1).toString() + ".pdf"
+        let filename = "/tmp/file" + (i+1).toString() + ".pdf"
 
         let elem = urls[i];
-        let pdfContent = await request.get({uri: elem});
+        let pdfContent = await request.get({
+            uri: elem,
+            encoding: null
+        });
 
         try {
-            fs.writeFileSync(pdf_file, pdfContent);
+            if (fs.existsSync(filename)){
+                fs.unlinkSync(filename);
+            }
+            fs.writeFileSync(filename, pdfContent, "binary");
         } catch(err) {
             console.log(err);
         }
-        
-        fs.readFile(pdf_file, function(err, data) {
-            console.log("data: " , data);
-            console.log("\n");
-        });
 
-        fs.unlink(pdf_file, function(err) {
+        let processor = await pdf_extract(filename, options, function(err) {
             if (err) console.log(err);
         });
+
+        processor.on('complete', function(data) {
+            console.log("pdf data: ", data.text_pages);            
+        });
+
+        processor.on('error', function(err) {
+            console.log(err);
+        });
+
+
+        // TODO ENSURE FILES ARE DELETED!!
+        /*
+        fs.unlink(filename, function(err) {
+            if (err) console.log(err);
+        });*/
 
         /*
         crawler(elem).then(function(response){
