ed0cf5a3df158ca03fac6b6604c2ea0f9877425b
Andre Rocha <andrefmrocha@live.com.pt>
2019-08-03 17:04:18 +0100

    Added tests for the endpoint

---
+++ test/date_and_cantine_endpoint_tests.js
@@ -0,0 +1,38 @@
+require("../web/index");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const mealsRepository = require("../lib/repository/meals_repository");
+chai.use(chaiHttp);
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+describe("Testing get endpoint", function(){
+    let meal = {
+        soup: "batata frita",
+        meat: "Vitela assada",
+        fish: "Sardinhas",
+        veggie: "Salada",
+        cantine: "FEUP",
+        day: "1995-12-17",
+        lunch: true
+    };
+    const baseUrl = `http://localhost:${PORT}`;
+    mealsRepository.save(meal);
+    it("should return a meal", function(){
+        chai
+            .request(baseUrl)
+            .get(`/meals/${meal.day}/${meal.cantine}`)
+            .end((err, result) => {
+                result.should.have.status(200);
+            });
+    });
+    it("should NOT return a meal", function(){
+        chai
+            .request(baseUrl)
+            .get(`/meals/${meal.day}/${meal.cantine}2`)
+            .end((err, result) => {
+                result.should.have.status(404);
+            });
+    });
+
+});
